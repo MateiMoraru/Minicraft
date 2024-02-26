5d27098d6ed472d6f8c9dca9900605c03fccd637
MateiMoraru <moraru.matei24@gmail.com>
2024-02-26 23:06:26 +0200

    Spawn command

--- commands.py
+++ commands.py
@@ -1,4 +1,5 @@
-from spritesheet import ID_STR
+from spritesheet import *
+from zombie import Zombie
 
 
 class Commands:
@@ -19,6 +20,7 @@ class Commands:
                 value = int(command[2])
             except:
                 print("ERROR: Excpected set <argument: str>, <value: int>")
+                return
             if arg == "time":
                 self.main.environment.time = value
                 print(f"INFO: Set current time to {value}")
@@ -31,6 +33,7 @@ class Commands:
                 amount = int(command[2])
             except:
                 print("ERROR: Excpected set <block: int>, <amount: int>")
+                return
             self.main.environment.player.inventory.add_item([block, amount])
             print(f"INFO: Added {ID_STR(block)} (amount: {amount}) to player's inventory")
         elif command[0] == "clear":
@@ -38,9 +41,28 @@ class Commands:
                 target = command[1]
             except:
                 print("ERROR: Expected target argument")
+                return
             if target == "entities":
                 self.main.environment.entities = [[], []]
                 print(f"INFO: Cleared all the entities")
             elif target == "items":
                 self.main.environment.ground_items = [[], []]
                 print(f"INFO: Cleared all the ground items")
+        elif command[0] == "spawn": ### TODO: Make it dynamic
+            try:
+                entity = command[1]
+            except:
+                print("ERROR: Expected entity name")
+                return
+
+            cave = self.main.environment.cave_level
+            window = self.main.window
+            spritesheet = self.main.spritesheet
+            sfx = self.main.environment.sfx
+            pos = self.main.environment.player.player.pos
+            offset = self.main.environment.player.offset
+            pos = [pos[0] + offset[0], pos[1] + offset[1]]
+            size = self.main.environment.sprite_size
+            texture_id = ZOMBIE
+            name = "ZOMBIE"
+            self.main.environment.entities[cave].append(Zombie(window, spritesheet, sfx, pos, size))
